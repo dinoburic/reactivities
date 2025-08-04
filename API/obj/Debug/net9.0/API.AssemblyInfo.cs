77c9f4ed5078f5c43e3d98c59fdef756dfb82157
Dino Burić <165225286+nodib202@users.noreply.github.com>
2025-08-05 01:50:03 +0200

    Implemented migrations

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4feaa6988f8dfd510117b2afc848bcb51f47ac16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f7ab2d6db1891a0eff564e0cfa75fbc069a583")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
