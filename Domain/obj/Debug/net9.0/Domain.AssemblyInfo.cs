fcfca316652c5bf4ec5c539f4d335764b57e676d
Dino Burić <165225286+nodib202@users.noreply.github.com>
2025-08-09 00:13:04 +0200

    Added Create and UpdateActivity

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e90656d6d3ec6995b6e088fd4a481ec27fa48b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac980d141701977ca41394a16d27ad2962f8a905")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
